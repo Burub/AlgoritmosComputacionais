b580a2fabe6cecc2d83a83bca6ae7dba6202f9b9
Brub <37320128+Burub@users.noreply.github.com>
2024-11-23 16:17:26 -0300

    Trabalho 1

---
+++ Trabalhos/4.c
@@ -0,0 +1,14 @@
+//4.Escreva um programa que leia uma temperatura em graus Farenheit e a imprima na tela em graus Centígrados. A conversão de graus Farenheit para Centígrados é obtida por c=(5/9)(f-32)
+#include <stdio.h>
+int main() {
+    float f, c;
+    
+    printf("Digite a temperatura em Farenheit: ");
+    scanf("%f", &f);
+    
+    f = f - 32;
+    c = (5/9) * f;
+
+    printf("%.2f F é igual a %.2f C", f, c);
+    return 0;
+}
